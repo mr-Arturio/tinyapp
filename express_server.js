c3ec98b0ce1feb74f1528488180fe8ee47761e6f
ArturT <mr.arturio@gmail.com>
2023-03-02 20:40:48 -0500

    new route to render template to display the long URL and its shortened form

--- express_server.js
+++ express_server.js
@@ -2,25 +2,28 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-// set the view engine to ej
+// set the view engine to ejs
 app.set("view engine", "ejs");
 
+//short URLs and their corresponding long URLs
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http;//www.google.com'
+  '9sm5xK': 'http://www.google.com'
 };
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
-
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
+app.get('/urls/:id', (req, res) => {
+  const templateVars = {
+    id: req.params.id, longURL: 'http://www.lighthouselabs.ca'};
+  res.render('urls_show', templateVars);
 });
 
 
